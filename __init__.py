418fcd59b21075158d70963afbbcdb2b4a904aeb
Mayur Pande <mayurpande.uk@gmail.com>
2017-08-10 14:18:58 +0100

    Set up init file, import flask and render template, created view for landing page

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 14:12:49 2017
+
+@author: mayur
+"""
+
+from flask import Flask, render_template, url_for
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def landing_page():
+    return render_template('landing.html')
